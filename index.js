975c90df95a4e25c52adaa96222403bc6cb158dc
dmacdo02 <dmacdo02@gmail.com>
2016-12-29 12:38:45 -0500

    actually fixing tabs vs spaces

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ var m = {
 	},
 	// success
 	s: function(msg) {
-    logUtil.logWithColor(chalk.bold.green, msg);
+		logUtil.logWithColor(chalk.bold.green, msg);
 	},
 	// warning
 	w: function(msg) {
-    logUtil.logWithColor(chalk.bgYellow.black, msg);
+		logUtil.logWithColor(chalk.bgYellow.black, msg);
 	},
 	// err
 	e: function(msg) {
@@ -27,15 +27,15 @@ var m = {
 	},
 	// highlight
 	h: function(msg) {
-    logUtil.logWithColor(chalk.bgCyan.black, msg);
+		logUtil.logWithColor(chalk.bgCyan.black, msg);
 	},
 	//info
 	i: function(msg) {
-    logUtil.logWithColor(chalk.bold.cyan, msg);
+		logUtil.logWithColor(chalk.bold.cyan, msg);
 	},
 	//trace:
 	t: function(msg) {
-    logUtil.logTraceWithColor(chalk.green, msg);
+		logUtil.logTraceWithColor(chalk.green, msg);
 	},
 
 	///////////
@@ -44,19 +44,19 @@ var m = {
 
 	// default
 	line: function(char, length) {
-    linesUtil.logLine(char, length, this.e, this.w);
+		linesUtil.logLine(char, length, this.e, this.w);
 	},
 	// default - another
 	dline: function(char, length) {
-    linesUtil.logLine(char, length, this.e, this.w);
+		linesUtil.logLine(char, length, this.e, this.w);
 	},
 	// success
 	sline: function(char, length) {
-    linesUtil.logLine(char, length, this.e, this.w, chalk.bold.green);
+		linesUtil.logLine(char, length, this.e, this.w, chalk.bold.green);
 	},
 	// warning
 	wline: function(char, length) {
-    linesUtil.logLine(char, length, this.e, this.w, chalk.bgYellow.black);
+		linesUtil.logLine(char, length, this.e, this.w, chalk.bgYellow.black);
 	},
 	// error
 	eline: function(char, length) {
@@ -64,7 +64,7 @@ var m = {
 	},
 	// highlight
 	hline: function(char, length) {
-    linesUtil.logLine(char, length, this.e, this.w, chalk.bgCyan.black);
+		linesUtil.logLine(char, length, this.e, this.w, chalk.bgCyan.black);
 	}
 };
 
