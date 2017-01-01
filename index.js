ba9c12a515c2c60f9320814807a0dcf467b1fd84
dmacdo02 <dmacdo02@gmail.com>
2017-01-01 13:56:01 -0500

    adding options of using custom colors instead of default ones

--- index.js
+++ index.js
@@ -1,71 +1,92 @@
 'use strict';
-
 var chalk = require('chalk');
 var logUtil = require('./logUtil');
 var linesUtil = require('./linesUtil');
+var extend = require('util')._extend;
 
-var m = {
-	//////////////////
-	// main methods //
-	//////////////////
-
-	// default
-	d: function(msg) {
-		logUtil.logWithColor(chalk.white, msg);
-	},
-	// success
-	s: function(msg) {
-		logUtil.logWithColor(chalk.bold.green, msg);
-	},
-	// warning
-	w: function(msg) {
-		logUtil.logWithColor(chalk.bgYellow.black, msg);
-	},
-	// err
-	e: function(msg) {
-		logUtil.logWithColor(chalk.bgRed.white, msg);
-	},
-	// highlight
-	h: function(msg) {
-		logUtil.logWithColor(chalk.bgCyan.black, msg);
-	},
-	//info
-	i: function(msg) {
-		logUtil.logWithColor(chalk.bold.cyan, msg);
-	},
-	//trace:
-	t: function(msg) {
-		logUtil.logTraceWithColor(chalk.green, msg);
-	},
-
-	///////////
-	// lines //
-	///////////
-
-	// default
-	line: function(char, length) {
-		linesUtil.logLine(char, length, this.e, this.w);
-	},
-	// default - another
-	dline: function(char, length) {
-		linesUtil.logLine(char, length, this.e, this.w);
-	},
-	// success
-	sline: function(char, length) {
-		linesUtil.logLine(char, length, this.e, this.w, chalk.bold.green);
-	},
-	// warning
-	wline: function(char, length) {
-		linesUtil.logLine(char, length, this.e, this.w, chalk.bgYellow.black);
-	},
-	// error
-	eline: function(char, length) {
-		linesUtil.logLine(char, length, this.e, this.w, chalk.bgRed.white);
-	},
-	// highlight
-	hline: function(char, length) {
-		linesUtil.logLine(char, length, this.e, this.w, chalk.bgCyan.black);
-	}
+var defaultColors = {
+	d: chalk.white,
+	s: chalk.bold.green,
+	w: chalk.bgYellow.black,
+	e: chalk.bgRed.white,
+	h: chalk.bgCyan.black,
+	i: chalk.bold.cyan,
+	t: chalk.green
 };
+
+function moduleSetup(colors) {
+	return {
+		//////////////////
+		// main methods //
+		//////////////////
+
+		// default
+		d: function(msg) {
+			logUtil.logWithColor(colors.d, msg);
+		},
+		// success
+		s: function(msg) {
+			logUtil.logWithColor(colors.s, msg);
+		},
+		// warning
+		w: function(msg) {
+			logUtil.logWithColor(colors.w, msg);
+		},
+		// err
+		e: function(msg) {
+			logUtil.logWithColor(colors.e, msg);
+		},
+		// highlight
+		h: function(msg) {
+			logUtil.logWithColor(colors.h, msg);
+		},
+		//info
+		i: function(msg) {
+			logUtil.logWithColor(colors.i, msg);
+		},
+		//trace:
+		t: function(msg) {
+			logUtil.logTraceWithColor(colors.t, msg);
+		},
+
+		///////////
+		// lines //
+		///////////
+
+		// default
+		line: function(char, length) {
+			linesUtil.logLine(char, length, this.e, this.w);
+		},
+		// default - another
+		dline: function(char, length) {
+			linesUtil.logLine(char, length, this.e, this.w);
+		},
+		// success
+		sline: function(char, length) {
+			linesUtil.logLine(char, length, this.e, this.w, colors.s);
+		},
+		// warning
+		wline: function(char, length) {
+			linesUtil.logLine(char, length, this.e, this.w, colors.w);
+		},
+		// error
+		eline: function(char, length) {
+			linesUtil.logLine(char, length, this.e, this.w, colors.e);
+		},
+		// highlight
+		hline: function(char, length) {
+			linesUtil.logLine(char, length, this.e, this.w, colors.h);
+		}
+	}
+}
+
+function customColors(options) {
+  //lets use the default colors, unless otherwise specified (options override)
+	var colors = extend(defaultColors, options);
+	return moduleSetup(colors);
+}
+
+var m = moduleSetup(extend({}, defaultColors));
+m.customColors = customColors;
 
 module.exports = m;
--- test.js
+++ test.js
@@ -9,6 +9,10 @@ node test.js
 */
 
 const lme = require('./index');
+var chalk = require('chalk');
+
+var newColors = {i: chalk.bgRed.white};
+const lmeColors = lme.customColors(newColors);
 
 // objects
 lme.d({ kity: "is pinky", fluffy: "is funny!" });
@@ -58,4 +62,6 @@ lme.wline({ yu: 1 });
 lme.eline({ yu: 1 });
 lme.hline({ yu: 1 });
 
+lmeColors.h('should use default highlight color');
+lmeColors.i('This color should be different than below.');
 lme.i('test finished');
