75ae23883265711bd046ea670ae7fbd7fee810e8
Derek MacDonald <Derek.MacDonald@d2l.com>
2017-01-07 10:06:27 -0500

    Adding some error handling to building chalk functions.

--- config.js
+++ config.js
@@ -1,29 +1,41 @@
 var chalk = require('chalk');
 
-function getChalkColors(configInput) {
+function getChalkColors(defaultConfig, overrideConfig) {
   // make the read file understandable to chalk.
+  var chalkColors = buildChalkFunction(defaultConfig);
+  var overrideChalkColors = buildChalkFunction(overrideConfig);
+  return Object.assign({}, chalkColors, overrideChalkColors);
+}
+
+function buildChalkFunction(config) {
   var chalkColors = {};
-  for (var color in configInput.colors) {
-  	if (configInput.colors.hasOwnProperty(color)) {
-  		var colorFunc = chalk;
-      //build up the chalk function
-      configInput.colors[color].forEach(function(style) {
-  			colorFunc = colorFunc[style];
-  		});
-  		chalkColors[color] = colorFunc;
-  	}
+  for (var color in config.colors) {
+      if (config.colors.hasOwnProperty(color)) {
+        var colorFunc = chalk;
+        //build up the chalk function
+        try {
+          config.colors[color].forEach(function(style) {
+            colorFunc = colorFunc[style];
+          });
+          if (typeof colorFunc === 'undefined') {
+            throw new Error('Unsupported colorFunc');
+          }
+          chalkColors[color] = colorFunc;
+        } catch(err) {
+          console.log(`Error setting style for ${color}. Will use default style.`);
+        }
+      }
   }
   return chalkColors;
 }
 
 var configInput = require("./lmeDefaultConfig.json");
+var providedConfig = {};
 try {
 	// try to get the config file from root file of the application.
-	var providedConfig = require('app-root-path').require("lmeconfig.json");
-  configInput.colors = Object.assign({}, configInput.colors, providedConfig.colors);
+	providedConfig = require('app-root-path').require("lmeconfig.json");
 } catch (e) {
 	//it is okay if no override file is provided
 }
-
-configInput.colors = getChalkColors(configInput);
+configInput.colors = getChalkColors(configInput, providedConfig);
 module.exports = configInput;
