7a04c6a3a31572424dbe8b4b2295bb94762decb4
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-01-28 23:07:10 +0530

    Initial attempt to add line-colors - Initial attempt to add line-colors - and modify the config format

--- src/loggingFunctions.js
+++ src/loggingFunctions.js
@@ -9,14 +9,14 @@ Object.keys(config._logLevels).forEach(function(logLevel) {
 		logFunction: logLevel === 'trace' ? logUtil.logTraceWithColor : logUtil.logWithColor,
 		level: config._logLevels[logLevel].level,
 		functionName: config._logLevels[logLevel].letter,
-		color: config.colors[logLevel],
+		color: config._types.log === 0 ? config.colors.logs[logLevel] : config.colors.lines[logLevel],
 		type: config._types.log
 	});
 	loggingFunctions.push({
 		logFunction: logLevel === 'trace' ? linesUtil.logLineWithTrace : linesUtil.logLine,
 		level: config._logLevels[logLevel].level,
 		functionName: config._logLevels[logLevel].letter + 'line',
-		color: config.colors[logLevel],
+		color: config._types.log === 0 ? config.colors.logs[logLevel] : config.colors.lines[logLevel],
 		type: config._types.line
 	});
 });
@@ -26,7 +26,7 @@ loggingFunctions.push({
 	logFunction: linesUtil.logLine,
 	level: config._logLevels.default.level,
 	functionName: 'line',
-	color: config.colors.default,
+	color: config.colors.lines.default,
 	type: config._types.line
 });
 
