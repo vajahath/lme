031f0f715f4b4bce43c52d509f863119753f3e19
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-02-04 11:46:52 +0530

    testing

--- tests/verifying_config_files/index.js
+++ tests/verifying_config_files/index.js
@@ -1,6 +1,59 @@
-// confirming config files are working correctly
+//////////////////
+// sample tests //
+//////////////////
+/**
+
+run:
+node test.js
+
+*/
+
 var lme = require('lme');
 
-// lmeconfig.json file is added with a different default color.
-// So, now the output should be in the new color.
-lme.d('hi');
+function testAllLogFunctions() {
+	var args = Array.prototype.slice.call(arguments);
+	lme.d.apply(lme, args);
+	lme.s.apply(lme, args);
+	lme.w.apply(lme, args);
+	lme.e.apply(lme, args);
+	lme.h.apply(lme, args);
+	lme.i.apply(lme, args);
+	lme.t.apply(lme, args);
+	lme.d('\n');
+}
+
+function testAllLineFunctions() {
+	var args = Array.prototype.slice.call(arguments);
+	lme.line.apply(lme, args);
+	lme.dline.apply(lme, args);
+	lme.sline.apply(lme, args);
+	lme.wline.apply(lme, args);
+	lme.eline.apply(lme, args);
+	lme.hline.apply(lme, args);
+	lme.iline.apply(lme, args);
+	lme.tline.apply(lme, args);
+}
+
+// objects
+testAllLogFunctions({ kity: 'is pinky', fluffy: 'is funny!' });
+
+// strings
+testAllLogFunctions('mango is sweet!');
+
+//Multiple Params
+testAllLogFunctions('mango is sweet!', { mango: 'sweet' });
+testAllLogFunctions('mango is sweet!', '!!!');
+testAllLogFunctions({ 'mango is': 'sweet' }, { 'mango is': 'sweet' });
+
+
+// lines
+testAllLineFunctions();
+testAllLineFunctions('*', 10);
+testAllLineFunctions('*', 110);
+
+// error-ed outputs for lines
+lme.i('\nerror-ed outputs');
+
+testAllLineFunctions({ yu: 1 });
+
+lme.i('test finished');
