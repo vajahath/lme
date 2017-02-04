dc51b8082545e2cb4ac403b722b759b4109b45e7
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-02-04 11:37:19 +0530

    Fixing the bug with line colors

--- src/loggingFunctions.js
+++ src/loggingFunctions.js
@@ -9,14 +9,14 @@ Object.keys(config._logLevels).forEach(function(logLevel) {
 		logFunction: logLevel === 'trace' ? logUtil.logTraceWithColor : logUtil.logWithColor,
 		level: config._logLevels[logLevel].level,
 		functionName: config._logLevels[logLevel].letter,
-		color: config._types.log === 0 ? config.colors.logs[logLevel] : config.colors.lines[logLevel],
+		color: config.colors.logs[logLevel],
 		type: config._types.log
 	});
 	loggingFunctions.push({
 		logFunction: logLevel === 'trace' ? linesUtil.logLineWithTrace : linesUtil.logLine,
 		level: config._logLevels[logLevel].level,
 		functionName: config._logLevels[logLevel].letter + 'line',
-		color: config._types.log === 0 ? config.colors.logs[logLevel] : config.colors.lines[logLevel],
+		color: config.colors.lines[logLevel],
 		type: config._types.line
 	});
 });
